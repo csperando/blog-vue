a0535d0ff8537968477dff494c73e8a4603eb67d
Coleman Sperando <csperando@gmail.com>
2024-09-17 15:00:38 -0600

    post tile component

---
+++ src/components/postTile.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <div>
+        <img/>
+
+        <div>
+            <span>Timestamp</span>
+            <p><b>Title</b></p>
+            <p><em>Short description</em></p>
+        </div>
+    </div>
+</template>
