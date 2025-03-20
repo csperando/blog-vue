1dd5e5175b8ad2a40bad994e16e26652ca07210f
Coleman Sperando <csperando@gmail.com>
2025-03-20 14:38:00 -0600

    test

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
 
 export const base_path = import.meta.env.VITE_BASE_PATH || "https://csperando-blog-rest-frdyhdcjh9ddfhb2.eastus-01.azurewebsites.net";
 // export const base_path = "https://csperando-blog-rest-frdyhdcjh9ddfhb2.eastus-01.azurewebsites.net";
-
-// export const this_base = import.meta.env.VITE_THIS_BASE_PATH || "/blog-vue/#/";
