35f942710d6709264705ad642d5322094151aece
csperando <csperando@gmail.com>
2025-04-25 12:33:08 -0600

    test

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,14 +1,7 @@
 <script setup>
-import progressTabs from "../components/progressTabs.vue";
-
-const progressBarData = [ "Metas", "Series", "Content" ];
-
-const test = function(e) {
-    console.log(e);
-}
 
 </script>
 
 <template>
-    <progressTabs :data="progressBarData" @update="test"></progressTabs>
+    <p>empty</p>
 </template>
